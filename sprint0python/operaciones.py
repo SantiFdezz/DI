72412097b32e9dca8d7396319a64e91fb932aa42
Santiago Fernandez <sfernandezs22@fpcoruna.afundacion.org>
2023-09-27 10:10:28 +0200

    Completado Ejercicio 3 Python, operaciones.py

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,11 @@
+def suma (i, j):
+    return i + j 
+def resta (i,j):
+    return i - j
+def multip (i,j):
+    return i * j
+def div (i,j):
+    if j != 0:
+        return i / j
+    else: 
+        print("No se puede divir entre 0.")
