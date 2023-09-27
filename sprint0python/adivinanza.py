d72dcd382e6afd863365629dc608aea941ccb6ac
Santiago Fernandez <sfernandezs22@fpcoruna.afundacion.org>
2023-09-27 09:55:48 +0200

    Ejercicio 2 Python, Arreglado

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,17 +1,17 @@
 list_dic=[
         {
                 'key': 'Son doce señoras con medias, pero sin zapatos. ¿De quiénes se trata?',
-                'answ': [{'A': 'EL VIENTO'}, {'B': 'EL MAPA'}, {'C': 'LAS HORAS DE UN RELOJ'}],
+                'answ': ['A) El viento', 'B) El mapa', 'C) Las horas del un reloj'],
                 'correct': 'C'
         },
         {
                 'key': 'Puede silbar sin labios y correr sin pies. Además, te pega en la espalda sin que lo puedas ver.',
-                'answ': [{'A': 'EL VIENTO'}, {'B': 'EL MAPA'}, {'C': 'LAS HORAS DE UN RELOJ'}],
+                'answ': ['A) El viento', 'B) El mapa', 'C) Las horas del un reloj'],
                 'correct': 'A'
         },
         {
                 'key': '¿Dónde hay ríos sin agua, ciudades sin casas y bosques sin árboles?',
-                'answ': [{'A': 'EL VIENTO'}, {'B': 'EL MAPA'}, {'C': 'LAS HORAS DE UN RELOJ'}],
+                'answ': ['A) El viento', 'B) El mapa', 'C) Las horas del un reloj'],
                 'correct': 'B'
         }
 ]
@@ -31,7 +31,7 @@ for clave in list_dic:
         if opcion == list_dic[i]['correct'] :
                 score+=10
         else:
-                score+=5
+                score-=5
         i+=1
 
 print("La puntuación total es de: {}".format(score))
