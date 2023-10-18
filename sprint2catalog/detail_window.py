521dace0320b5b0a629a3c5c6611e611184aced7
Santiago Fernandez <sfernandezs22@fpcoruna.afundacion.org>
2023-10-18 10:35:10 +0200

    Ejercicio 6 completado, MenuBarAyuda

--- sprint2catalog/detail_window.py
+++ sprint2catalog/detail_window.py
@@ -7,7 +7,7 @@ def showDetail(cell):
     det_root.title(cell.title)
     x = (det_root.winfo_screenwidth() - det_root.winfo_reqwidth()) / 2 ##screenwidth devuelve el ancho de la pantalla,  y reqwidth nos devuelve la anchura en pixels de la ventana.
     y = (det_root.winfo_screenheight() - det_root.winfo_reqheight()) / 2
-    det_root.geometry(f"+{int(x)}+{int(y)}")
+    det_root.geometry(f"+{int(x)}+{int(y)}") # f lo centra
     label = ttk.Label(det_root, image=cell.image_tk, text=cell.title, compound= tk.BOTTOM) ## label imagen titulo
     label2 = ttk.Label(det_root, text=cell.description) ## label de descripcion
     label.pack()
--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -1,4 +1,4 @@
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import tkinter as tk
 from cell import Cell
 from detail_window import showDetail
@@ -12,6 +12,11 @@ class MainWindow:
         y = (self.root.winfo_screenheight() - self.root.winfo_reqheight()) / 2
         self.root.geometry(f"+{int(x)}+{int(y)}") # Esto crea el tamaño por defecto de la ventana
         #self.root.resizable(False, False) #No le dejamos redimensionar x ni y
+        barras_menu = tk.Menu()
+        menu_Ayuda = tk.Menu(barras_menu, tearoff=False)## creamos el primer menú
+        menu_Ayuda.add_command(label="Acerca de",command=self.show_info) ##añadimos al menu Ayuda la entrada Acerca De
+        barras_menu.add_cascade(menu=menu_Ayuda, label="Ayuda")
+        self.root.config(menu=barras_menu)
         for  animal in json_data:
             name = animal.get("name")
             description = animal.get("description")
@@ -23,4 +28,7 @@ class MainWindow:
             label = ttk.Label(self.root, image=cell.image_tk, text=cell.title, compound= tk.BOTTOM) ## Imagen y label title
             label.grid(row=i,column=0)
             label.bind("<Button-1>",lambda event, cell = cell: showDetail(cell)) 
-
+    
+    def show_info(self):
+        messagebox.showinfo("Acerca De ", "Desarollador: Fdezz1t0")
+        
